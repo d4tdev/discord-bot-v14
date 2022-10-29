1f208d556f311d2f99970403329ded4a545b2bba
Nguyễn Hồng Đức <contact.hongduc@gmail.com>
2022-10-29 15:02:07 +0700

    Update xsmb

--- Commands/Public/Info/xsmb.js
+++ Commands/Public/Info/xsmb.js
@@ -5,6 +5,16 @@ const {
 } = require('discord.js');
 const axios = require('axios');
 const Ascii = require('ascii-table');
+const award = {
+   G_ĐB: "Giải đặc biệt",
+   G_1: "Giải nhất",
+   G_2: "Giải nhì",
+   G_3: "Giải ba",
+   G_4: "Giải tư",
+   G_5: "Giải năm",
+   G_6: "Giải sáu",
+   G_7: "Giải bảy",
+};
 
 module.exports = {
    data: new SlashCommandBuilder()
@@ -16,27 +26,29 @@ module.exports = {
     * @param {Client} client
     */
    execute(interaction, client) {
+      interaction.reply({ content: 'Đang tải kết quả...' });
       axios.get('https://api-xsmb-today.herokuapp.com/v1').then(res => {
          const data = res.data;
+
+         let arrTimesNames = Object.keys(data.objTimesNames).map((key) => ({
+            time: key,
+            name: data.objTimesNames[key],
+         }));
 
          const table = new Ascii(`Kết quả XSMB ngày: ${data.time}`);
          table.setBorder('|', '-', '+', '+');
          table.setHeading('Giải', 'Kết quả');
-         table.addRow('Giải Đặc Biệt', data.objTimesNames.G_ĐB);
-         table.addRow('Giải nhất', data.objTimesNames.G_1);
-         table.addRow('Giải nhì', formatArray(data.objTimesNames.G_2));
-         table.addRow('Giải ba', formatArray(data.objTimesNames.G_3));
-         table.addRow('Giải tư', formatArray(data.objTimesNames.G_4));
-         table.addRow('Giải năm', formatArray(data.objTimesNames.G_5));
-         table.addRow('Giải sáu', formatArray(data.objTimesNames.G_6));
-         table.addRow('Giải bảy', formatArray(data.objTimesNames.G_7));
+         
+         arrTimesNames.forEach((item) => {
+            table.addRow(award[item.time], item.name);
+         });
 
          table.setAlign(0, Ascii.CENTER);
          table.setAlign(1, Ascii.CENTER);
 
          const embed = new EmbedBuilder()
             .setAuthor({
-               name: 'Kết quả xổ số miền Bắc',
+               name: 'Kết quả xổ số miền Bắc host',
                iconURL: 'https://image.vietstock.vn/2022/01/07/220107_XS_1.jpg',
             })
             .setColor('#2a9454')
@@ -44,13 +56,13 @@ module.exports = {
             .setFooter({
                text: 'Powered by d4rtj#8572',
             });
-         interaction.reply({ embeds: [embed] });
+         interaction.editReply({ content: "", embeds: [embed] });
       });
    },
 };
 
-function formatArray(arr) {
-   return arr.map((item, index) => {
-      return `${item}`;
-   });
-}
+// function formatArray(arr) {
+//    return arr.map((item, index) => {
+//       return `${item}`;
+//    });
+// }
