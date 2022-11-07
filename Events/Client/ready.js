761a6d02c5e754268e54b8d5bb085c9dd32d7f78
DDatj <jsmsj04@gmail.com>
2022-11-08 01:19:40 +0700

    update status

--- Events/Client/ready.js
+++ Events/Client/ready.js
@@ -9,22 +9,34 @@ module.exports = {
       await loadCommands(client);
       console.log(`${client.user.tag} is ready!`);
       const statuses = [
-         { name: 'dd4tj#8572 🔥', type: ActivityType.Listening },
-         { name: `by ${client.guilds.cache.size} servers 📺`, type: ActivityType.Watching },
-         { name: `by ${client.users.cache.size} users 👨‍💻`, type: ActivityType.Watching },
+         {
+            name: `by ${client.guilds.cache.size} servers 📺`,
+            type: ActivityType.Watching,
+         },
+         {
+            name: `by ${client.users.cache.size} users 👨‍💻`,
+            type: ActivityType.Watching,
+         },
          {
             name: `by ${client.channels.cache.size} channels 🎞`,
             type: ActivityType.Watching,
          },
+         { name: ` /help ⚙️`, type: ActivityType.Listening },
+         // { name: `${client.user.username} 🤖`, type: ActivityType.Watching },
+         { name: ` /help ⚙️`, type: ActivityType.Listening },
+
+         { name: 'dd4tj#8572 🔥', type: ActivityType.Listening },
          { name: 'by Slash Commands [/]', type: ActivityType.Playing },
-         { name: ' /help ⚙️', type: ActivityType.Listening },
       ];
 
-      client.user.setPresence({ activities: statuses[0], status: 'online' });
-      let index = 1;
+      client.user.setPresence({
+         activities: [{ name: 'dd4tj#8572 🔥', type: ActivityType.Listening }],
+         status: 'online',
+      });
+      let index = 0;
       setInterval(() => {
-         if (index > 5) index = 0;
-
+         if (index >= statuses.length ) index = 0;
+         
          client.user.setActivity(statuses[index]);
          index++;
       }, 3500);
