aac6ffb37116baa1e389c9e4868cc1ebc71692ee
DDatj <jsmsj04@gmail.com>
2022-11-09 12:27:44 +0700

    create serverlist for dev

---
+++ SlashCommands/Developer/Info/serverlist.js
@@ -0,0 +1,46 @@
+const {
+   SlashCommandBuilder,
+   EmbedBuilder,
+   PermissionFlagsBits,
+   ChatInputCommandInteraction,
+} = require('discord.js');
+
+const ErrorHandler = require('../../../Functions/errorHandler');
+
+module.exports = {
+   category: 'Developer',
+   developer: true,
+   data: new SlashCommandBuilder()
+      .setName('server-list')
+      .setDescription('Get list of servers that bot is in.')
+      .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+   /**
+    * @param {ChatInputCommandInteraction} interaction
+    * @param {Client} client
+    **/
+   async execute(interaction, client) {
+      try {
+         let arrays = [];
+         client.guilds.cache.forEach(guild => {
+            arrays.push(
+               `🔹 Name: **${guild.name}** - Owner: <@${guild.ownerId}> - Members Count: **${guild.memberCount}**`
+            );
+         });
+
+         await interaction.reply({
+            embeds: [
+               new EmbedBuilder()
+                  .setAuthor({
+                     name: 'Danh sách các server mà bot đang ở',
+                     iconURL: client.user.avatarURL(),
+                  })
+                  .setColor('2a9454')
+                  .setTitle(`Tổng số server: ${client.guilds.cache.size}`)
+                  .setDescription(arrays.map(array => array).join('\n')),
+            ],
+         });
+      } catch (err) {
+         ErrorHandler(interaction, err);
+      }
+   },
+};
